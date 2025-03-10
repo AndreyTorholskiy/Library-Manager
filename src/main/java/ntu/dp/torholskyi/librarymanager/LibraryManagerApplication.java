c3cc3ac2e0a5c6d963b4adae6aa4846a9e4a967f
AndriiTorholskyi <Torholskyi.An.O@nmu.one>
2025-03-10 14:48:23 +0200

    upfating code

--- src/main/java/ntu/dp/torholskyi/librarymanager/LibraryManagerApplication.java
+++ src/main/java/ntu/dp/torholskyi/librarymanager/LibraryManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LibraryManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LibraryManagerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagerApplication.class, args);
+    }
 
 }
